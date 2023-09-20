a66095c5c7d605a732a355b1670707acca6cc656
JuSTC3 <thejustc3@gmail.com>
2023-09-20 10:49:02 +0530

    personal Digital Assistant

---
+++ pda/src/ProfileCard.js
@@ -0,0 +1,22 @@
+function ProfileCard({ title, handle, image, desc }) {
+    return (
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt={title} />
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p classname="subtitle is-6">{handle}</p>
+                </div>
+                <div className="content">
+                    {desc}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProfileCard;
