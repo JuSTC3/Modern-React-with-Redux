2e0125ac24f511a26714876c76c618a3f90792b2
JuSTC3 <thejustc3@gmail.com>
2023-09-23 16:44:55 +0530

    context implemented

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -2,11 +2,12 @@ import { useContext } from 'react';
 import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
-function BookList({ books, onDelete, onEdit }) {
-    const {count, incrementCount} = useContext(BooksContext);
+function BookList() {
+    const { books } = useContext(BooksContext);
+
     const renderedBooks = books.map((book) => {
         return (
-            <BookShow onEdit={onEdit} key={book.id} book={book} onDelete={onDelete} />
+            <BookShow key={book.id} book={book} />
         );
     });
 
