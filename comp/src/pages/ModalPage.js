fa1a4f62c934eff63325a704ea79068584385101
JuSTC3 <thejustc3@gmail.com>
2023-10-07 20:42:27 +0530

    modal

--- comp/src/pages/ModalPage.js
+++ comp/src/pages/ModalPage.js
@@ -9,10 +9,28 @@ function ModalPage() {
         setShowModal(true);
     };
 
+    const handleClose = () => {
+        setShowModal(false);
+    };
+
+    const actionBar = (
+        <div>
+            <Button onClick={handleClose} primary>I Accept</Button>
+        </div>
+    );
+
+    const modal = (
+        <Modal onClose={handleClose} actionBar={actionBar}>
+            <p>
+                Here is an important agreement for you to accept
+            </p>
+        </Modal>
+    );
+
     return (
         <div>
             <Button onClick={handleClick}>Open Modal</Button>
-            {showModal && <Modal />}
+            {showModal && modal}
         </div>
     );
 }
