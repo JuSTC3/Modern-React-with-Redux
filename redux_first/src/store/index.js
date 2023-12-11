a51ecf7afe61443fbc397e2850e84e777fe39472
JuSTC3 <thejustc3@gmail.com>
2023-12-11 12:05:19 +0530

    redux base

---
+++ redux_first/src/store/index.js
@@ -0,0 +1,35 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const songsSlice = createSlice({
+    name: 'song',
+    initialState: [],
+    reducers: {
+        addSong(state, action) {
+            state.push(action.payload);
+        },
+        removeSong(state, action) {
+            //
+        }
+    }
+});
+
+const store = configureStore({
+    reducer: {
+        songs: songsSlice.reducer
+    }
+});
+
+// console.log(songsSlice.actions.addSong());
+
+// const startingState = store.getState();
+// console.log(JSON.stringify(startingState));
+
+// store.dispatch(
+//     songsSlice.actions.addSong('New Song!!!')
+// );
+
+// const finalState = store.getState();
+// console.log(JSON.stringify(finalState));
+
+export { store };
+export const { addSong } = songsSlice.actions;
